83f3c68a3ff29fe1dbb010d0f36b31232ba65339
Stuart Neilson <stuart.neilson@gmail.com>
2021-03-01 16:32:33 -0500

    versioning

--- src/csci_utils/__init__.py
+++ src/csci_utils/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.0.0"
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = "0.0.0"
